75552e850664a9fb8b1262d45f0711fb761731f9
Amber <windjiayu@gmail.com>
2024-01-07 15:19:13 +1100

    add EJS partial views

--- controllers/skills.js
+++ controllers/skills.js
@@ -3,12 +3,14 @@ const Skill = require("../models/skill");
 function index (req, res){
     res.render("skills/index", {
         skills: Skill.getAll(),
+        heading: "Developer Skills",
     })
 }
 
 function getOne (req, res){
     res.render("skills/skill", {
         skill: Skill.getOne(req.params.id),
+        heading: "Skills Details"
 
     })
 }
